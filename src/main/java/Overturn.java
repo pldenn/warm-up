c60d2f6bf6940192852d7605e221f11c66be3a69
Denis Timofeev <y719xy@gmail.com>
2020-09-04 14:35:02 +0300

    add test for method reverseString

--- src/main/java/Overturn.java
+++ src/main/java/Overturn.java
@@ -2,11 +2,11 @@ public class Overturn {
 
     public String reverseString (String rev) {
         char[] array = rev.toCharArray();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = array.length - 1; i >=0 ; i--) {
-            result += array[i];
+            result.append(array[i]);
         }
-        return result;
+        return result.toString();
     }
 
 }
---
+++ src/test/java/OverturnTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OverturnTest {
+
+    @Test
+    public void shouldRevers(){
+    Overturn rev = new Overturn();
+        String actual = "george";
+        Assertions.assertEquals("egroeg", rev.reverseString(actual));
+    }
+
+}
